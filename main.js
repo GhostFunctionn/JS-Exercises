60e4cf790cff41349e892b30cae0b32074788d8d
GhostFunctionn <83043061+GhostFunctionn@users.noreply.github.com>
2022-12-09 01:20:26 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+// Exercise 1 - Log Your Country
+let myCountry = "England";
+console.log(myCountry);
+
+// Exercise 2 -
+console.log(myCountry.length);
+
+//Exercise 3
+let favouriteFood = "Spaghetti";
+favouriteFood = favouriteFood.toUpperCase();
+console.log(favouriteFood);
+
+// Exercise 4
+favouriteFood = favouriteFood.toLowerCase();
+console.log(favouriteFood);
+
+// Exercise 5
+let myName = "Justin";
+let age = 28;
+
+let message =
+  "Hello, my name is " + myName + " and I am " + age + " years old.";
+
+console.log(message);
+
+console.log("What is your name? \b " + message);
+
+//Exercise 6
+let firstName = "Justin";
+let lastName = "Humphreys";
+
+console.log(`Welcome ${firstName} ${lastName}. How can I help you?`);
